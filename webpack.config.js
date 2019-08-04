f9c721721719452df617eae14c2af800a99e61fd
Austin <aconnor731@gmail.com>
2019-08-04 13:55:08 -0500

    fix package.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
